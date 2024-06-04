8170deb82acb4dbf26ba7fa1855c7da488b213eb
Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
2024-06-04 06:18:10 -0700

    added valkey-module and hello command

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[macro_use]
+extern crate valkey_module;
+
+use valkey_module::{alloc::ValkeyAlloc, Context, Status, ValkeyResult, ValkeyString, ValkeyValue};
+
+fn initialize(ctx: &Context, _args: &[ValkeyString]) -> Status {
+    ctx.log_notice("initialize rustmod");
+    Status::Ok
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn deinitialize(ctx: &Context) -> Status {
+    ctx.log_notice("deinitialize rustmod");
+    Status::Ok
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn hello(_ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
+    Ok(ValkeyValue::SimpleStringStatic("world"))
+}
+
+valkey_module! {
+    name: "rustmod",
+    version: 1,
+    allocator: (ValkeyAlloc, ValkeyAlloc),
+    data_types: [],
+    init: initialize,
+    deinit: deinitialize,
+    commands: [
+        ["rustmod.hello", hello, "", 0, 0, 0],
+    ]
 }
