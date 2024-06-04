24b9a005da71bc8d8113ed456abcadee59070f54
Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
2024-06-04 06:35:32 -0700

    added setget command

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 #[macro_use]
 extern crate valkey_module;
 
-use valkey_module::{alloc::ValkeyAlloc, Context, Status, ValkeyResult, ValkeyString, ValkeyValue};
+use valkey_module::{
+    alloc::ValkeyAlloc, Context, NextArg, Status, ValkeyError, ValkeyResult, ValkeyString,
+    ValkeyValue,
+};
 
 fn initialize(ctx: &Context, _args: &[ValkeyString]) -> Status {
     ctx.log_notice("initialize rustmod");
@@ -17,6 +20,21 @@ fn hello(_ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
     Ok(ValkeyValue::SimpleStringStatic("world"))
 }
 
+fn setget(ctx: &Context, args: Vec<ValkeyString>) -> ValkeyResult {
+    if args.len() < 3 {
+        return Err(ValkeyError::WrongArity);
+    }
+    let mut args = args.into_iter().skip(1);
+    let key = args.next_str().unwrap();
+    let value = args.next_str().unwrap();
+    // write
+    let _ = ctx.call("set", &[key, value]);
+    // read
+    let resp = ctx.call("get", &[key])?;
+    ctx.log_notice(&format!("resp: {:?}", resp));
+    Ok(resp)
+}
+
 valkey_module! {
     name: "rustmod",
     version: 1,
@@ -26,5 +44,6 @@ valkey_module! {
     deinit: deinitialize,
     commands: [
         ["rustmod.hello", hello, "", 0, 0, 0],
+        ["rustmod.setget", setget, "", 0, 0, 0],
     ]
 }
